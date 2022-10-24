a0314902aeb1046f50e4b795b307a9fb00868531
Derek Zhou <derek@3qin.us>
2022-10-24 01:22:39 +0000

    remove pointless refactoring

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,8 @@
  import {forwardItem, backwardItem, deleteItem, refreshItem, subscribe, unsubscribe,
 	 clearData, saveFeeds, restoreFeeds} from  './airss_controller.js';
 
- import Content from './Content.svelte';
+ import { afterUpdate } from 'svelte';
+
  // constants
  // screen is fundimental content shown in the window
  const Screens = {
@@ -90,6 +91,41 @@
  $: screen = !$running ? Screens.shutdown : screen;
  $: alertClass = alertClassFromType($alertType);
  $: dummy = isDummyItem($currentItem);
+
+ afterUpdate(() => {
+     if (!$currentItem)
+	 return;
+     let container = document.querySelector("#content_html");
+     let baseUrl = $currentItem.url;
+     if (!container || dummy)
+	 return;
+     try {
+	 let url = new URL(baseUrl);
+     } catch (e) {
+	 console.warn(baseUrl + " is not a valid url");
+	 return;
+     }
+     // fix up all img's src
+     for (let img of container.querySelectorAll("img").values()) {
+	 let href = img.getAttribute("src");
+	 try {
+	     let absUrl = new URL(href, baseUrl);
+	     img.setAttribute("src", absUrl.toString());
+	 } catch (e) {
+	     console.warn(href + "is not a valid link");
+	 }
+     }
+     // fixup all a's href
+     for (let link of container.querySelectorAll("a").values()) {
+	 let href = link.getAttribute("href");
+	 try {
+	     let absUrl = new URL(href, baseUrl);
+	     link.setAttribute("href", absUrl.toString());
+	 } catch (e) {
+	     console.warn(href + "is not a valid link");
+	 }
+     }
+ });
 
  // view functions
 
@@ -295,7 +331,45 @@
 		  | {$currentItem.datePublished.toLocaleString()}
 	      </span>
 	  </h5>
-	  <Content contentHtml={$currentItem.contentHtml} baseUrl={$currentItem.url}/>
+	  {/if}
+	  <div id="content_html"
+	       class="font-serif text-gray-800 leading-snug mb-2 sm:text-lg sm:leading-relaxed">
+	      {#if $currentItem}
+		  {@html $currentItem.contentHtml}
+	      {:else}
+		  <h2>No news is bad news</h2>
+		  <p>
+		      Airss is a web feed reader that runs entirely in your browser. You can subscribe any feeds by clicking the 🍼 button from above and paste the URL, or you can use of one of the following tricks: 
+		  </p>
+		  <h3>Desktop browser users</h3>
+		  <p>
+		      Install this bookmarklet <a class="button" href="javascript:location.href='{airssPrefix}?url='+encodeURIComponent(window.location.href)">Subscribe it in Airss</a> <b>by dragging it to your bookmarks</b>. Whenever you encounter something interesting on the web, be it a blog, a news website or whatever, you can click this bookmarklet to subscribe. Chances are they support RSS feeds so you will always stay updated.
+		  </p>
+		  <h3>Mobile browser users</h3>
+		  <p>
+		      Android users can install this APP: <a href="https://f-droid.org/en/packages/net.daverix.urlforward/">URL Forwarder</a> (Thank you, David Laurell!) then add a filter as:
+		  </p>
+		  <pre>{airssPrefix}?url=@url</pre>
+		  <p>
+		      Then you can share links to the APP and select the menu to subscribe, if it support RSS feeds.
+		  </p>
+		  <p>
+		      iOS Safari users can use the bookmarklet method as mentioned earlier by syncing the bookmarklet from your Mac.
+		  </p>
+		  <h2>To my fellow bloggers</h2>
+		  <p>
+		      Please make sure you have your feed <a href="https://www.rssboard.org/rss-autodiscovery">auto-discoverable</a> from your homepage. And if you can, please enable <a href="https://enable-cors.org/">permissive CORS</a> on your blog to reach out to a broader audience. Lastly, if you really like Airss, you can put a link on your homepage:
+		  </p>
+		  <pre>&lt;a href="{airssPrefix}?subscribe-referrer"
+referrerpolicy="no-referrer-when-downgrade"&gt;
+Follow me with Airss!
+&lt;/a&gt;</pre>
+		  <p>
+		      So your readers can have an even easier time to follow you.
+		  </p>
+	      {/if}
+	  </div>
+	  {#if $currentItem}
 	  <div class="flex flex-wrap w-full gap-x-1 justify-center">
 	      <button class="button py-1 px-6 inline-block rounded appearance-none font-bold
 			     text-lg text-center border-0 text-white bg-pink-600"
@@ -308,40 +382,6 @@
 			     text-lg text-center border-0 text-white bg-purple-600" target="roast"
 		     href={roastPrefix + encodeURIComponent($currentItem.url)}>🔥</a>
 	      {/if}
-	  </div>
-	  {:else}
-	  <div id="content_html"
-	       class="font-serif text-gray-800 leading-snug mb-2 sm:text-lg sm:leading-relaxed">
-	      <h2>No news is bad news</h2>
-	      <p>
-		  Airss is a web feed reader that runs entirely in your browser. You can subscribe any feeds by clicking the 🍼 button from above and paste the URL, or you can use of one of the following tricks: 
-	      </p>
-	      <h3>Desktop browser users</h3>
-	      <p>
-		  Install this bookmarklet <a class="button" href="javascript:location.href='{airssPrefix}?url='+encodeURIComponent(window.location.href)">Subscribe it in Airss</a> <b>by dragging it to your bookmarks</b>. Whenever you encounter something interesting on the web, be it a blog, a news website or whatever, you can click this bookmarklet to subscribe. Chances are they support RSS feeds so you will always stay updated.
-	      </p>
-	      <h3>Mobile browser users</h3>
-	      <p>
-		  Android users can install this APP: <a href="https://f-droid.org/en/packages/net.daverix.urlforward/">URL Forwarder</a> (Thank you, David Laurell!) then add a filter as:
-	      </p>
-	      <pre>{airssPrefix}?url=@url</pre>
-	      <p>
-		  Then you can share links to the APP and select the menu to subscribe, if it support RSS feeds.
-	      </p>
-	      <p>
-		  iOS Safari users can use the bookmarklet method as mentioned earlier by syncing the bookmarklet from your Mac.
-	      </p>
-	      <h2>To my fellow bloggers</h2>
-	      <p>
-		  Please make sure you have your feed <a href="https://www.rssboard.org/rss-autodiscovery">auto-discoverable</a> from your homepage. And if you can, please enable <a href="https://enable-cors.org/">permissive CORS</a> on your blog to reach out to a broader audience. Lastly, if you really like Airss, you can put a link on your homepage:
-	      </p>
-	      <pre>&lt;a href="{airssPrefix}?subscribe-referrer"
-referrerpolicy="no-referrer-when-downgrade"&gt;
-Follow me with Airss!
-&lt;/a&gt;</pre>
-	      <p>
-		  So your readers can have an even easier time to follow you.
-	      </p>
 	  </div>
 	  {/if}
       </div>
