739bace465604be08312f816bf72c0424ec2f409
Derek Zhou <derek@3qin.us>
2023-02-02 03:10:38 +0000

    add babel, which is needed for solidjs to work

--- snowpack.config.js
+++ snowpack.config.js
@@ -10,16 +10,13 @@ module.exports = {
 	"public": "/"
     },
     plugins: [
+	'@snowpack/plugin-babel',
 	"@snowpack/plugin-postcss"
     ],
     routes: [
 	/* Enable an SPA Fallback in development: */
 	// {"match": "routes", "src": ".*", "dest": "/index.html"},
     ],
-    optimize: {
-	/* Example: Bundle your final build: */
-	// "bundle": true,
-    },
     packageOptions: {
 	/* ... */
     },
